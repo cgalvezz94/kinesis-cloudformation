506fd6edd08e579b20b308d8d227497c634c6b58
Camilo <camilogalvez10@gmail.com>
2025-08-29 11:36:24 -0400

    [Add]: schema added

---
+++ app/schemas/binance_trade_schema.py
@@ -0,0 +1,17 @@
+# schemas/binance_trade_schema.py
+
+BINANCE_TRADE_SCHEMA = {
+    "type": "object",
+    "properties": {
+        "e": {"type": "string"},
+        "E": {"type": "integer"},
+        "s": {"type": "string"},
+        "t": {"type": "integer"},
+        "p": {"type": "string"},
+        "q": {"type": "string"},
+        "T": {"type": "integer"},
+        "m": {"type": "boolean"},
+        "M": {"type": "boolean"}
+    },
+    "required": ["e", "E", "s", "t", "p", "q", "T", "m", "M"]
+}
